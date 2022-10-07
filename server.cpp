67a72c014f52e8743883daaedf72d40f90b20207
Lazy Hippopotamus <lazy@hippo.com>
2022-10-07 12:18:21 -0400

    Set up server stuff, database still unchecked

--- server.cpp
+++ server.cpp
@@ -1,39 +1,22 @@
 #include <httplib.h>
-
-/*
-void respondFile(httplib::Response* res, std::string filename, std::string type = "text/html"){
-    std::ifstream file;
-    file.open(filename);
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    std::string fileData = buffer.str();
-    res -> set_content(buffer.str(), type);
-}*/
+#define CROW_ENFORCE_WS_SPEC
+#include "crow_all.h"
 
 
 int main(){
-    httplib::Server svr;/*
-    svr.Get("/", [](const httplib::Request &, httplib::Response &res) {
-        respondFile(&res, "index.html");
-    });
-    svr.Get("/index.html", [](const httplib::Request &, httplib::Response &res) {
-        respondFile(&res, "index.html");
-    });
-    svr.Get("/main.css", [](const httplib::Request &, httplib::Response &res){
-        respondFile(&res, "main.css", "text/css");
-    });
-    svr.Get("/main.js", [](const httplib::Request &, httplib::Response &res){
-        respondFile(&res, "main.js", "application/javascript");
-    });
-    svr.Get("/README.md", [](const httplib::Request &, httplib::Response &res){
-        res.set_content("Stop it. Just stop it.", "text/plain");
-    });
-    svr.Get("/admin", [](const httplib::Request &, httplib::Response &res) {
-        respondFile(&res, "face.txt");
-    });*/
+    crow::SimpleApp app;
+    CROW_ROUTE(app, "/")
+    .websocket()
+    .onopen([&](crow::websocket::connection& conn){
 
+    }).onclose([&](crow::websocket::connection& conn, std::string reason){
+
+    }).onmessage([&](crow::websocket::connection& conn, std::string data, bool is_binary){
+        conn.send_text(data);
+    });
+    app.port(9002).multithreaded().run();
+    httplib::Server svr;
     svr.set_mount_point("/", "./pub");
-
     svr.listen("0.0.0.0", 8080);
     return 0;
 }
