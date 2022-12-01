b9be8f32d25d689269e130672614952a1152105e
Rowboat Sammy <boujie@debian.linuxrocks2000>
2022-12-01 10:33:23 -0500

    update build.sh and gitignore

--- server/build.sh
+++ server/build.sh
@@ -1,1 +1,1 @@
-g++ main.cpp -g -lpthread -lssl -lcrypto -Wall -DCROW_ENABLE_SSL -DCROW_FEATURES="ssl;compression" -DCROW_BUILD_EXAMPLES=OFF -DCROW_BUILD_TESTS=OFF
+g++ main.cpp -g -lpthread -lssl -lcrypto -Wall -DCROW_ENABLE_SSL -DCROW_FEATURES="ssl;compression" -DCROW_BUILD_EXAMPLES=OFF -DCROW_BUILD_TESTS=OFF -o castleblocks
